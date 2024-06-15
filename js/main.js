cb6e0cd3f79cefd3e1fcf65c227772eb37637ae6
Thông Trần Quang <tranquangthong03012005@gmail.com>
2024-06-15 23:37:57 +0700

    The end of upload code to git

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
 
-// var swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     autoplay: {
-//       delay: 2500,
-//       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-
-//   var swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 30,
-//     effect: "fade",
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
 
 // Swiper
 var swiper = new Swiper(".mySwiper", {
